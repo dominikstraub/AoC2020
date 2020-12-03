6a83ea8452f870e51786545a7ff1d03c36e16de7
Dominik Straub <dominik.straub@gmail.com>
2020-12-03 07:47:52 +0100

    Day3

--- Sources/Day3/main.swift
+++ Sources/Day3/main.swift
@@ -4,10 +4,42 @@ import Utils
 // let input = try Utils.getInput(day: 3, file: "test.txt")
 let input = try Utils.getInput(day: 3)
 
-let items = input
+let field = input
     .components(separatedBy: CharacterSet(charactersIn: "\n"))
+    .map { (item) -> [Bool]? in
+        if item == "" {
+            return nil
+        }
+        return Array(item)
+            .map { $0 == "#" }
+    }
     .compactMap { $0 }
 
-print("Part 1: \(part1(items: items))")
+func part1(field: [[Bool]], slope: Double = 3) -> Int {
+    var trees = 0
+    for i in 0..<field.count {
+        let s = Double(i) * slope
+        if s.truncatingRemainder(dividingBy: 1) != 0 {
+            continue
+        }
+        let line = field[i]
+        if line[Int(s) % line.count] == true  {
+            trees += 1
+        }
+    }
+    
+    return trees
+}
 
-print("Part 2: \(part2(items: items))")
+print("Part 1: \(part1(field: field))")
+
+func part2(field: [[Bool]]) -> Int {
+    let slopes = [1, 3, 5, 7, 0.5]
+    var result = 1
+    for slope in slopes {
+        result *= part1(field: field, slope: slope)
+    }
+    return result
+}
+
+print("Part 2: \(part2(field: field))")
