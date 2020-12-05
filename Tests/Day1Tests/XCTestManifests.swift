6a171e12c2e64e1401397d2c62050a12ded9be58
Dominik Straub <dominik.straub@gmail.com>
2020-12-05 07:34:18 +0100

    removed not needed tests

--- Tests/Day1Tests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(Day1Tests.allTests),
-    ]
-}
-#endif
--- Tests/LinuxMain.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-
-import Day1Tests
-
-var tests = [XCTestCaseEntry]()
-tests += Day1Tests.allTests()
-tests += Day2Tests.allTests()
-XCTMain(tests)
