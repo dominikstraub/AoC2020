4cf3c0cc33bcbb202fc48f2ae52112cff79b330f
Dominik Straub <dominik.straub@gmail.com>
2020-12-28 14:44:06 +0100

    Day23, Part1

--- Sources/Day23/main.swift
+++ Sources/Day23/main.swift
@@ -1,17 +1,92 @@
 import Foundation
 import Utils
 
-let input = try Utils.getInput(bundle: Bundle.module, file: "test")
-// let input = try Utils.getInput(bundle: Bundle.module)
+// let input = try Utils.getInput(bundle: Bundle.module, file: "test")
+let input = try Utils.getInput(bundle: Bundle.module)
 
 let cups = input
     .components(separatedBy: "\n")[0]
     .compactMap { Int(String($0)) }
 
-print(cups)
+// print(cups)
 
-func part1() -> Int {
-    return -1
+struct CrabCups {
+    var cups: [Int]
+    var currentCup: Int
+    var pickedUp: [Int] = []
+
+    var currentCupIndex: Int {
+        cups.firstIndex(of: currentCup)!
+    }
+
+    var destinationCup: Int {
+        var destinationCup = currentCup
+        while true {
+            destinationCup = (destinationCup - 1) %% (cups.max()! + 1)
+            if cups.contains(destinationCup) {
+                return destinationCup
+            }
+        }
+    }
+
+    var destinationCupIndex: Int {
+        cups.firstIndex(of: destinationCup)!
+    }
+
+    var nextCup: Int {
+        cups[nextCupIndex]
+    }
+
+    var nextCupIndex: Int {
+        (currentCupIndex + 1) % cups.count
+    }
+
+    var order: String {
+        cups.split(separator: 1).reversed().map { $0.map { "\($0)" }.joined() }.joined()
+    }
+
+    mutating func placeCups() {
+        cups.insert(contentsOf: pickedUp, at: destinationCupIndex + 1)
+        pickedUp.removeAll()
+    }
+
+    mutating func pickUp() {
+        pickedUp.append(cups.remove(at: nextCupIndex))
+    }
+
+    mutating func pickUp(times: Int) {
+        for _ in 0 ..< times {
+            pickUp()
+        }
+    }
+
+    mutating func move() {
+        // print("cups: \(cups)")
+        pickUp(times: 3)
+        // print("pick up: \(pickedUp)")
+        // print("destination: \(destinationCup)")
+        placeCups()
+        currentCup = nextCup
+    }
+
+    mutating func move(moves: Int) {
+        for _ in 0 ..< moves {
+            // print("\n-- move \(moveNr + 1) --")
+            move()
+        }
+    }
+
+    init(cups: [Int]) {
+        self.cups = cups
+        currentCup = self.cups.first!
+    }
+}
+
+func part1() -> String {
+    var game = CrabCups(cups: cups)
+    game.move(moves: 100)
+    // print(game.cups)
+    return game.order
 }
 
 print("Part 1: \(part1())")
